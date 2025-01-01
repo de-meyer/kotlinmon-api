0f39e44f17d9d63cb48471c6f77d417f21c1abfc
de-meyer <dmeyer.private@gmail.com>
2025-01-01 23:43:49 +0100

    initial commit

---
+++ src/main/kotlin/com/api/kotlinmon/kotlinmonapi/KotlinmonApiApplication.kt
@@ -0,0 +1,11 @@
+package com.api.kotlinmon.kotlinmonapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinmonApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinmonApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/kotlinmon/kotlinmonapi/KotlinmonApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.kotlinmon.kotlinmonapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinmonApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
