e1e19d6a657b2515c493094bbb4a476a879e4ff1
de-meyer <dmeyer.private@gmail.com>
2025-01-12 22:47:48 +0100

    test: add exception test

--- src/test/kotlin/com/api/kotlinmon/kotlinmonapi/PokemonControllerTest.kt
+++ src/test/kotlin/com/api/kotlinmon/kotlinmonapi/PokemonControllerTest.kt
@@ -1,10 +1,12 @@
 package com.api.kotlinmon.kotlinmonapi
 
+import com.api.kotlinmon.kotlinmonapi.exceptions.ResourceNotFoundException
 import com.api.kotlinmon.kotlinmonapi.models.Pokemon
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 @WebMvcTest(PokemonController::class)
@@ -22,6 +24,14 @@ class PokemonControllerTest {
     fun shouldReturnPokemonById() = runBlocking {
         val pokemon: Pokemon = pokemonController.getPokemon("25")
         assertEquals("pikachu", pokemon.name)
+    }
+
+    @Test
+    fun shouldReturnPokemonByInvalidName() = runBlocking {
+        val exception = assertThrows<ResourceNotFoundException> {
+            pokemonController.getPokemon("invalid")
+        }
+        assertEquals("Pokemon not found with name/id: invalid", exception.message)
     }
 
 }
